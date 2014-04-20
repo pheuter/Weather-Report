718a6025aa769faa689fe9bfa95bdd1234727637
Mark Fayngersh <mark.fayngersh@gmail.com>
2014-04-20 11:32:11 -0400

    Added TTS

--- app/src/main/java/com/marfay/weatherreport/app/MainActivity.java
+++ app/src/main/java/com/marfay/weatherreport/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.marfay.weatherreport.app;
 import android.app.Activity;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 import com.google.android.glass.app.Card;
@@ -10,6 +11,7 @@ import com.google.android.glass.app.Card;
 
 public class MainActivity extends Activity {
 
+  private TextToSpeech mTTS;
   private RequestQueue mQueue;
   private Card mCard;
 
@@ -17,10 +19,15 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+      @Override
+      public void onInit(int status) {
+      }
+    });
     mQueue = Volley.newRequestQueue(this);
     mCard = new Card(this);
 
-    String cityName = getIntent().getExtras().getStringArrayList(RecognizerIntent.EXTRA_RESULTS).get(0);
+    final String cityName = getIntent().getExtras().getStringArrayList(RecognizerIntent.EXTRA_RESULTS).get(0);
     if (cityName == null) {
       mCard.setText("Could not parse city name, please try again!");
       setContentView(mCard.getView());
@@ -35,6 +42,7 @@ public class MainActivity extends Activity {
             .setFootnote(weatherData.temperature + "° Fahrenheit");
 
         setContentView(mCard.getView());
+        mTTS.speak(String.format("%s in %s at %d degrees Fahrenheit", weatherData.description, cityName, weatherData.temperature), TextToSpeech.QUEUE_ADD, null);
       }
     });
   }
